0937149211d9f19280234bad32982940850ec26e
NavyaKM <NavyaKM@ANT018.Moxiesoft.corp>
2020-02-01 13:02:27 +0530

    done

---
+++ MVN/src/test/java/com/actitime/Newtest/CreateTestNGUsere.java
@@ -0,0 +1,16 @@
+package com.actitime.Newtest;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateTestNGUsere 
+{
+	
+	@Test
+	public void testcustomer()
+	{
+	    Reporter.log("create test customer",true);
+	}
+	
+
+}
